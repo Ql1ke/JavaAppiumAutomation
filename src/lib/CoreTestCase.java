7bb7aa20274530889925f5c4b956cd28f4b2d9ad
Ql1ke <81801565+Ql1ke@users.noreply.github.com>
2021-07-14 16:47:23 +0300

    Lesson 5 ex.1

---
+++ src/lib/CoreTestCase.java
@@ -0,0 +1,40 @@
+package lib;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import junit.framework.TestCase;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.URL;
+
+public class CoreTestCase extends TestCase
+{
+    protected AppiumDriver driver;
+    private static String AppiumURL = "http://127.0.0.1:4723/wd/hub";
+
+    @Override
+    public void setUp() throws Exception
+    {
+        super.setUp();
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        capabilities.setCapability("platformName","Android");
+        capabilities.setCapability("deviceName","AndroidTestDevice");
+        capabilities.setCapability("platformVersion","8.0");
+        capabilities.setCapability("automationName","Appium");
+        capabilities.setCapability("appPackage","org.wikipedia");
+        capabilities.setCapability("appActivity",".main.MainActivity");
+        capabilities.setCapability("app","C:/PostwinSoftware/GitHub/JavaAppiumAutomation/apks/org.wikipedia.apk");
+
+        driver = new AndroidDriver(new URL(AppiumURL), capabilities);
+    }
+
+    @Override
+    public void tearDown() throws Exception
+    {
+        driver.quit();
+
+        super.tearDown();
+    }
+}
